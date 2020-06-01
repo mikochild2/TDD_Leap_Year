1e42aed1c280a567af116624a1b32a1613d78a26
mikochild2 <mikochild2@gmail.com>
2020-06-01 15:15:01 -0400

    add code to leapyear.java to test cannot be divisable by 100 unless divisible by 400. fail

--- LeapYear.java
+++ LeapYear.java
@@ -2,7 +2,9 @@ public class LeapYear {
    public boolean processYear(int year) {
        if (year %4 ==0 && year%100 != 0) {
            return true;
-       }else {
+       }else if (year %4 ==0 && year%100 == 0 && year%400==0) {
+           return false;
+       } else {
            return false;
        }
    } 
