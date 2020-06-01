1e3f779f3e809b5cb60c09acb5b8deb3b6672ab1
mikochild2 <mikochild2@gmail.com>
2020-06-01 15:34:52 -0400

    refactor code with methods. failed

--- LeapYear.java
+++ LeapYear.java
@@ -1,11 +1,26 @@
 public class LeapYear {
    public boolean processYear(int year) {
-       if (year %4 ==0 && year%100 != 0) {
+       if (isDivisableBy4(year) && isNotDivisableBy100(year)) {
            return true;
-       }else if (year %4 ==0 && year%100 == 0 && year%400==0) {
+       }else if (isDivisableBy4(year) & isDivisableBy100And400(year)) {
            return true;
-       } else {
+       } else if (isDivisableBy4(year)){
+           return true;
+       } else{
            return false;
        }
    } 
+
+   //methods
+   public boolean isDivisableBy4(int year) {
+       return year % 4 == 0;
+   }
+
+   public boolean isNotDivisableBy100(int year){
+       return year%100 !=0;
+   }
+
+   public boolean isDivisableBy100And400(int year){
+       return year%100==0 && year%400 ==0;
+   }
 }
