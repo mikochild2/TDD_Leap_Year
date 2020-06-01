ddb76259e54dee9e381e75eee6cfa87c97b50b66
mikochild2 <mikochild2@gmail.com>
2020-06-01 15:16:17 -0400

    update return in leapyear.java so test passess

--- LeapYear.java
+++ LeapYear.java
@@ -3,7 +3,7 @@ public class LeapYear {
        if (year %4 ==0 && year%100 != 0) {
            return true;
        }else if (year %4 ==0 && year%100 == 0 && year%400==0) {
-           return false;
+           return true;
        } else {
            return false;
        }
